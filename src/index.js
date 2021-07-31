4e82a52f2f5f59be0aab1a3992bdfa6e19d33e6f
subin <subin9033@gmail.com>
2021-07-31 09:38:11 +0900

    cors 문제 해결

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import express from 'express'
 import cors from 'cors'
 import messagesRoute from './routes/messages.js'
-import usersRoute from './routes/users.js'
 import drawingsRoute from './routes/drawings.js'
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(
-  cors({
-    origin: 'http://localhost:7000',
-    credentials: true,
-  }),
-)
+app.use(cors());
 
 const routes = [...messagesRoute, ...drawingsRoute]
 routes.forEach(({ method, route, handler }) => {
